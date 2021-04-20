b1657d97537a8f04eb6621ce00c84da6be553ce2
Bernardo Duarte <bernardo.duarte@nubank.com.br>
2021-04-20 17:50:33 -0300

    aula 2 - finaliza home

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nlw_quiz/home/widgets/app_bar/app_bar_widget.dart';
+import 'package:nlw_quiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:nlw_quiz/home/widgets/quiz_card/quiz_card_widget.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -13,6 +15,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                LevelButtonWidget(label: 'Fácil'),
+                LevelButtonWidget(label: 'Médio'),
+                LevelButtonWidget(label: 'Difícil'),
+                LevelButtonWidget(label: 'Perito'),
+              ],
+            ),
+            SizedBox(height: 24),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+                children: [
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                  QuizCardWidget(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nlw_quiz/core/core.dart';
+
+class LevelButtonWidget extends StatelessWidget {
+  LevelButtonWidget({
+    Key? key,
+    required this.label,
+  })   : assert(['Fácil', 'Médio', 'Difícil', 'Perito'].contains(label)),
+        super(key: key);
+
+  final String label;
+
+  final config = {
+    'Fácil': {
+      'color': AppColors.levelButtonFacil,
+      'border': AppColors.levelButtonBorderFacil,
+      'textColor': AppColors.levelButtonTextFacil,
+    },
+    'Médio': {
+      'color': AppColors.levelButtonMedio,
+      'border': AppColors.levelButtonBorderMedio,
+      'textColor': AppColors.levelButtonTextMedio,
+    },
+    'Difícil': {
+      'color': AppColors.levelButtonDificil,
+      'border': AppColors.levelButtonBorderDificil,
+      'textColor': AppColors.levelButtonTextDificil,
+    },
+    'Perito': {
+      'color': AppColors.levelButtonPerito,
+      'border': AppColors.levelButtonBorderPerito,
+      'textColor': AppColors.levelButtonTextPerito,
+    },
+  };
+
+  Color get backgroundColor => config[label]!['color']!;
+  Color get borderColor => config[label]!['border']!;
+  Color get textColor => config[label]!['textColor']!;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(28),
+        border: Border.fromBorderSide(BorderSide(color: borderColor)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 26, vertical: 6),
+        child: Text(
+          label,
+          style: GoogleFonts.notoSans(
+            color: textColor,
+            fontSize: 13,
+          ),
+        ),
+      ),
+    );
+  }
+}
