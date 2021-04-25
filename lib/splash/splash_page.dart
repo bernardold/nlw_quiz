b1bcc9ddcf137701907f8d2d3b169f5ffca90352
Bernardo Duarte <bernardo.duarte@nubank.com.br>
2021-04-25 13:16:30 -0300

    aula 5 - Splash

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:nlw_quiz/core/app_gradients.dart';
 import 'package:nlw_quiz/core/app_images.dart';
+import 'package:nlw_quiz/home/home_page.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 1)).then((_) => Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        ));
+
     return Scaffold(
       body: Container(
         child: Center(
